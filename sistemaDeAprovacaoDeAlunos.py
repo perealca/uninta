090fe35d8de050ea35a6d85054a240fcdfb12ef7
Renan Carneiro <perealca17@gmail.com>
2026-02-07 20:32:04 -0300

    Update sistemaDeAprovacaoDeAlunos.py

--- sistemaDeAprovacaoDeAlunos.py
+++ sistemaDeAprovacaoDeAlunos.py
@@ -2,13 +2,13 @@ assiduidade = float(input("Digite a assiduidade (%): "))
 nota = float(input("Digite a nota (0 a 20): "))
 
 if assiduidade < 75:
-    situacao = "Reprovado"
+situacao = "Reprovado"
 else:
-    if nota <= 5:
-        situacao = "Reprovado"
-    elif nota < 10:
-        situacao = "Exame"
-    else:
-        situacao = "Aprovado"
+if nota <= 5:
+situacao = "Reprovado"
+elif nota < 10:
+situacao = "Exame"
+else:
+situacao = "Aprovado"
 
 print("Situação do aluno:", situacao)
