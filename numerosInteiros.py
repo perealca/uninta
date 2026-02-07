9fc2fcfcac2649937069765a152b910e6fe15efa
Renan Carneiro <perealca17@gmail.com>
2026-02-07 20:27:45 -0300

    Create numerosInteiros.py

---
+++ numerosInteiros.py
@@ -0,0 +1,18 @@
+maior = None
+menor = None
+
+for i in range(10):
+    numero = int(input(f"Digite o {i+1}º número: "))
+
+    if i == 0:
+        maior = numero
+        menor = numero
+    else:
+        
+        if numero > maior:
+            maior = numero
+        if numero < menor:
+            menor = numero
+
+print("Maior número:", maior)
+print("Menor número:", menor)
