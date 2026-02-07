dcc488c7f49e658c71944e4fbe21ea65c1e89aa5
Renan Carneiro <perealca17@gmail.com>
2026-02-07 20:32:43 -0300

    Update sistemaVendasECompras.py

--- sistemaVendasECompras.py
+++ sistemaVendasECompras.py
@@ -1,13 +1,13 @@
 valor_compra = float(input("Digite o valor da compra: R$ "))
 
 if valor_compra < 10:
-    lucro = 0.70
+lucro = 0.70
 elif valor_compra < 30:
-    lucro = 0.50
+lucro = 0.50
 elif valor_compra < 50:
-    lucro = 0.40
+lucro = 0.40
 else:
-    lucro = 0.30
+lucro = 0.30
 
 valor_venda = valor_compra + (valor_compra * lucro)
 
